eb861d53f596b68d62e9e2af3cc1cc2e463dd249
webyyl <33775005+webyyl@users.noreply.github.com>
2017-12-12 21:23:25 +0800

    add ratings scoller

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" v-el:ratings-wrapper>
   	<div class="ratings-content">
   		<div class="overview">
   			<div class="overview-left">
@@ -20,27 +20,94 @@
   				</div>
   				<div class="delivery-wrapper">
   					<span class="title">送达时间</span>
-  					<span class="delivery">{{seller.deliveryTime}}</span>
+  					<span class="delivery">{{seller.deliveryTime}}分钟</span>
   				</div>
   			</div>
- 
   		</div>
-  	</div>
-   
+	  	
+	  	<split></split>
+	  	<ratingselect :select-type="selectType" :only-content="onliyContent" :desc="desc" :ratings="ratings"></ratingselect>
+	  	<div class="rating-wrapper">
+	  		<ul>
+	  			<li v-for="rating in ratings" class="rating-item">
+	  				<div class="avatar"><img width="28" height="28" :src="rating.avatar"></div>
+	  				<div class="content">
+	  					<h1 class="name">{{rating.username}}</h1>
+	  					<div class="star-wrapper">
+	  						<star :size="24" :score="rating.score"></star>
+	  						<span class="delivery" v-show="rating.deliveryTime">
+	  							{{rating.deliveryTime}}
+	  						</span>
+	  					</div>
+	  					<p class="text">{{rating.text}}</p>
+	  					<div class="recommend" v-show="rating.recommend&&rating.recommend.length">
+	  						<span class="icon-thumb_up"></span>
+	  						<span v-for="item in rating.recommend">{{item}}</span>
+	  					</div>
+	  					<div class="time">
+	  						{{rating.rateTime | formatDate}}
+	  					</div>
+	  				</div>
+	  			</li>
+	  		</ul>
+	  	</div>
+    </div>
   </div>
 </template>
 
 <script>
+
 import star from 'components/star/star';
+import split from "components/split/split.vue";
+import ratingselect from "components/ratingselect/ratingselect.vue";
+import {formatDate} from '../../common/js/date.js';
+import BScoller from "better-scroll";
+const ERR_OK=0;
+const ALL=2;
 export default{
 	props:{
 		seller:{
 			type:Object
-
 		}
 	},
+	
+	data(){
+        return{
+        	ratings:[],
+            selectType:ALL,
+            onliyContent:true,
+            desc:{
+                all:'全部',
+                positive:'推荐',
+                negative:'吐槽'
+            }
+
+        }
+    },
+    created(){
+		this.$http.get('/api/ratings').then((response) => {
+                response = response.body;
+                if (response.errno === ERR_OK) {
+                    this.ratings = response.data;
+                    this.$nextTick(() => {
+                        this.scroll = new BScoller(this.$els.ratingsWrapper, {
+                            click: true,
+                            probeType:3
+                        });
+                    });
+                }
+            });
+	},
+	filters: {
+        formatDate(time) {
+            let date = new Date(time);
+            return formatDate(date, 'yyyy-MM-dd hh:mm');
+        }
+    },
 	components:{
-		star
+		star,
+		split,
+		ratingselect
 	}
 
 }
@@ -49,12 +116,12 @@ export default{
 
 <style rel="stylesheet/less" lang="less" scope>
 .ratings{
-	position: absolute;
-	top: 174px;
-	bottom: 0;
-	left: 0;
-	width: 100%;
-	overflow: hidden;
+    position: fixed;
+    top: 174px;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
 	.overview{
 		display: flex;
 		padding: 18px 0;
@@ -64,6 +131,10 @@ export default{
 			width: 137px;
 			border-right: 1px solid rgba(7,17,27,0.1);
 			text-align: center;
+			@media only screen and(max-width: 320px){
+				flex:0 0 120px;
+				width: 120px
+			}
 			.score{
 				margin-bottom: 6px;
 				line-height: 28px;
@@ -86,7 +157,10 @@ export default{
 		}
 		.overview-right{
 			flex: 1;
-			padding-left: 24px;
+			padding: 6px 0 6px 24px;
+			@media only screen and(max-width: 320px){
+				padding-left:6px;
+			}
 			.score-wrapper{	
 				margin-bottom:8px;
 				font-size: 0;
@@ -109,7 +183,6 @@ export default{
 					vertical-align: top;
 					font-size: 12px;
 					color: rgb(255,153,0);
-
 				}
 			}
 			.delivery-wrapper{
